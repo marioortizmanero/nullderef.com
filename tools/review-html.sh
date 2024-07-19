46d339360aca0dde25da62ea0011a946eb3968de
Mario Ortiz Manero <marioortizmanero@gmail.com>
2024-07-19 17:02:27 +0200

    fix tool

--- tools/review-html.sh
+++ tools/review-html.sh
@@ -1,20 +1,31 @@
 #!/usr/bin/env bash
 
 clone_url=$(git config --get remote.origin.url)
-tmp_dir=$(mktemp -d)
+tmp_dir=/tmp/nullderef-review-html
 original_dir=$PWD
 
 echo ">> Building local site"
 hugo
 
 echo ">> Building reference site"
+mkdir -p "$tmp_dir"
 cd "$tmp_dir" || exit 1
-git clone "$clone_url" "$tmp_dir"
+if ! [ -d "${tmp_dir}/.git" ]; then
+  git clone "$clone_url" "$tmp_dir"
+fi
+git checkout master
+git pull
 git submodule init
 git submodule update
 hugo
 
-echo ">> Showing differences"
-diff --color=always "${original_dir}/public" "${tmp_dir}/public" | less -R
-
-rm -rf "$tmp_dir"
+echo ">> Showing differences between $original_dir and $tmp_dir"
+diff \
+  --brief \
+  --recursive \
+  --no-dereference \
+  --new-file \
+  --no-ignore-file-name-case \
+  "${tmp_dir}/public" "${original_dir}/public" \
+  | grep -v '^$' \
+  | awk '{ print "git diff", $2, $4 }'
