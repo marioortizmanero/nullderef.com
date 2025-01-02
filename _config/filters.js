ae932c68de482e9954549b27efdaa1db3e4c9827
Mario Ortiz Manero <marioortizmanero@gmail.com>
2025-01-02 03:03:17 +0100

    sitemaaaaaaap

--- _config/filters.js
+++ _config/filters.js
@@ -7,57 +7,64 @@ import metadata from "../_data/metadata.js";
 const markdownIt = new MarkdownIt();
 
 export default function(eleventyConfig) {
-	eleventyConfig.addFilter("absUrl", (relUrl) => {
+  eleventyConfig.addFilter("absUrl", (relUrl) => {
     return new URL(relUrl, metadata.url).href;
   });
 
-	eleventyConfig.addFilter("readableDateShort", (dateString) => {
+  eleventyConfig.addFilter("readableDateShort", (dateString) => {
     const date = new Date(dateString);
     return new Intl.DateTimeFormat("en-US", {
       year: "numeric",
       month: "short",
     }).format(date);
-	});
+  });
 
-	eleventyConfig.addFilter("readableDateLong", (dateString) => {
+  eleventyConfig.addFilter("readableDateLong", (dateString) => {
     const date = new Date(dateString);
     return new Intl.DateTimeFormat("en-US", {
       year: "numeric",
       month: "long",
       day: "numeric",
     }).format(date);
-	});
+  });
 
-	eleventyConfig.addFilter("iso8601Date", (dateString) => {
+  eleventyConfig.addFilter("iso8601Date", (dateString) => {
     const date = new Date(dateString);
     return date.toISOString();
-	});
+  });
 
   eleventyConfig.addFilter("lastModified", (filePath) => {
     const stats = fs.statSync(filePath);
     return stats.mtime;
   });
 
-	eleventyConfig.addFilter("getTags", (tags) => {
-		return Object.keys(tags)
+  eleventyConfig.addFilter("getTags", (tags) => {
+    return Object.keys(tags)
       .filter(tag => !["all", "posts", "series"].includes(tag))
-      .map(tag => ({ name: tag, count: tags[tag].length }))
+      .map(tag => ({
+          name: tag,
+          count: tags[tag].length,
+          lastModified: new Date(Math.max(
+            ...tags[tag].map(post => fs.statSync(post.inputPath).mtime)
+          ))
+      }))
+      .filter(tag => tag.count > 0)
       .sort((t1, t2) => t2.count - t1.count);  // Descending
-	});
+  });
 
-	eleventyConfig.addFilter("getTagsInPost", (tags) => {
-		return tags.filter(tag => tag !== "posts");
-	});
+  eleventyConfig.addFilter("getTagsInPost", (tags) => {
+    return tags.filter(tag => tag !== "posts");
+  });
 
-	eleventyConfig.addFilter("getSeries", (series, name) => {
+  eleventyConfig.addFilter("getSeries", (series, name) => {
     return series.find(s => s.data.name === name);
-	});
+  });
 
-	eleventyConfig.addFilter("getPostsInSeries", (posts, seriesName) => {
+  eleventyConfig.addFilter("getPostsInSeries", (posts, seriesName) => {
     return posts
       .filter(s => s.data.series === seriesName)
       .sort((p1, p2) => p1.data.date - p2.data.date);  // Ascending
-	});
+  });
 
   eleventyConfig.addFilter("recentPosts", function (posts) {
     return posts
@@ -65,7 +72,7 @@ export default function(eleventyConfig) {
       .sort((p1, p2) => p2.data.date - p1.data.date);  // Descending
   });
 
-	eleventyConfig.addFilter("assertLengthUnder", (str, maxLength) => {
+  eleventyConfig.addFilter("assertLengthUnder", (str, maxLength) => {
     if (typeof str !== "string" || !Number.isInteger(maxLength)) {
       throw new Error("assertLengthUnder filter expects a string and integer");
     }
@@ -73,17 +80,17 @@ export default function(eleventyConfig) {
       throw new Error(`String has length of ${str.length}, which is above the expected maximum of ${maxLength}: ${str}`);
     }
     return str;
-	});
+  });
 
-	eleventyConfig.addFilter("raiseError", (str) => {
+  eleventyConfig.addFilter("raiseError", (str) => {
     throw new Error(`Validation failed in a template: ${str}`);
   });
 
-	eleventyConfig.addFilter("markdownify", (str) => {
+  eleventyConfig.addFilter("markdownify", (str) => {
     return markdownIt.renderInline(str);
   });
 
-	eleventyConfig.addFilter("strip_whitespace", (str) => {
+  eleventyConfig.addFilter("strip_whitespace", (str) => {
     return str.replace(/[\r\n\t]/g, ' ').trim();
   });
 
@@ -113,7 +120,7 @@ export default function(eleventyConfig) {
   });
 
 
-	eleventyConfig.addFilter("log", (obj) => {
+  eleventyConfig.addFilter("log", (obj) => {
     console.log(obj);
     console.log("------------------------");
   });
--- eleventy.config.js
+++ eleventy.config.js
@@ -15,6 +15,7 @@ import metadata from "./_data/metadata.js";
 export default async function(eleventyConfig) {
   // Drafts, see also _data/eleventyDataSchema.js
   eleventyConfig.addPreprocessor("drafts", "*", (data, content) => {
+    if (data.draft){return false}
     if (data.draft && process.env.ELEVENTY_RUN_MODE === "build") {
       return false;
     }
